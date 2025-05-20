aeeaff6944371fe5cf7bdaba6c78294d184a879c
Pankhi Mehta <pnkmht135@gmail.com>
2025-05-21 00:28:52 +0530

    basic movement and player sprites (tilemap sprites are place holder)

---
+++ scripts/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("Move_left", "Move_right")
+#	Flip sprite
+	if direction > 0:
+		animated_sprite.flip_h= false
+	elif direction<0:
+		animated_sprite.flip_h= true # change this so that it uses fresh spritesheet with proper shading
+	
+	# Play da correct animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("Idle")
+		else:
+			animated_sprite.play("Run")
+	else:
+		animated_sprite.play("Jump")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
